0325c2b1c9ec853ce0b4b5caeefceb2e357aba4b
ZavarinArtem <zavarin.artem@gmail.com>
2022-04-22 15:17:33 +0300

    Added _onchange_lot_info

--- stock_picking_info_lot/models/stock_move.py
+++ stock_picking_info_lot/models/stock_move.py
@@ -1,9 +1,7 @@
-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class StockMove(models.Model):
-
     _inherit = "stock.move"
 
     lot_info = fields.Char('Lot/Serial Number info')
@@ -13,9 +11,15 @@ class StockMove(models.Model):
          ('required', 'Required')],
         related='product_id.product_tmpl_id.lot_info_usage')
 
+    # this is not working as wanted
+    @api.onchange('lot_info')
+    def _onchange_lot_info(self):
+        for rec in self:
+            for move_line in rec.move_line_ids:
+                move_line.lot_info = rec.lot_info
+
 
 class StockMoveLine(models.Model):
-
     _inherit = "stock.move.line"
 
     lot_info = fields.Char('Lot/Serial Number info')
@@ -24,5 +28,3 @@ class StockMoveLine(models.Model):
          ('optional', 'Optional'),
          ('required', 'Required')],
         related='product_id.product_tmpl_id.lot_info_usage')
-
-
