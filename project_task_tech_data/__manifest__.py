7f02e55293e053d47936655cb8ff50ce53515885
ZavarinArtem <zavarin.artem@gmail.com>
2022-07-01 15:15:45 +0300

    Module init

---
+++ project_task_tech_data/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Technical data for project tasks",
+    "summary": "Adds technical data (date of creation and uid) of task views",
+    "version": "15.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://spoc.com.ua/",
+    "author": "Zavarin Artem, S.P.O.C.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/project_views.xml",
+    ],
+}
