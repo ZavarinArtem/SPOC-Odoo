a0436479c70f7129d33fcc905c598d3d94dc9d3e
ZavarinArtem <zavarin.artem@gmail.com>
2022-11-22 14:47:04 +0200

    product_vendor_code prepared for SPOC rep

--- product_vendor_code/__manifest__.py
+++ product_vendor_code/__manifest__.py
@@ -1,20 +1,18 @@
 {
     "name": "Vendor code",
     "summary": """
-        Vendor's code for product template""",
+        Vendor's Code For Product Template""",
     "description": """
-        Vendor's code for product template
+        Allows to store vendor's code for product.  
     """,
-    "author": "S.P.O.C.",
-    "website": "https://github.com/OCA/stock-logistics-workflow",
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    "category": "Uncategorized",
-    "version": "14.0.1.0.1",
-    # any module necessary for this one to work correctly
+    "author": "SPOC",
+    "website": "https://spoc-odoo.com.ua/",
+    'company': 'SPOC corp',
+    'maintainer': 'SPOC corp',
+    'license': 'LGPL-3',
+    "category": "Sales/Sales",
+    "version": "15.0.1.0.2",
     "depends": ["product"],
-    # always loaded
     "data": [
         "views/views.xml",
     ],
